f02ee8434fb0b79795cfe639462bd87e0a2bf3cd
perrotta3 <146884970+perrotta3@users.noreply.github.com>
2023-12-12 21:40:48 -0300

    Update first_streamlit_wine.py

--- first_streamlit_wine.py
+++ first_streamlit_wine.py
@@ -4,25 +4,25 @@ import plotly.express as px
 
 st.set_page_config(layout='wide')
 
-# importando a tabela
+# table import
 valor_import = pd.read_excel('https://github.com/perrotta3/first-streamlit/raw/49da611a630d53413dcdbd42cc9b32a81e9a5026/import.xlsx', sheet_name='valor', engine='openpyxl')
 
-# tirando a coluna id, pois não tem utilidade nessa análise
+# droping id column
 valor_import = valor_import.drop(columns='Id')
 
-# Reorganizando as colunas
+# rerranging the columns
 vl_melted = valor_import.melt(id_vars=['País'], var_name='Ano', value_name='Valor(US$)')
 valor = vl_melted[['Ano', 'País', 'Valor(US$)']]
 valor['Ano'] = pd.to_datetime(valor['Ano'], format='%Y')
 valor['Ano'] = valor['Ano'].dt.year
 
 
-# agrupando por ano para ver o valor anual
+# grouping by year
 valor_anual = valor.groupby(['Ano'])['Valor(US$)'].sum().reset_index()
 valor_anual['Ano'] = pd.to_datetime(valor_anual['Ano'], format='%Y')
 valor_anual['Ano'] = valor_anual['Ano'].dt.year
 
-# Função para obter os top 5 países por ano
+# function to get top 5 countries per year
 def top_countries_by_year(df):
     top_countries = (
         df.groupby('Ano')
@@ -31,32 +31,32 @@ def top_countries_by_year(df):
     )
     return top_countries
 
-# Aplicar a função para obter os top 5 países por ano
+
 top_countries_by_year(valor)
 top5 = top_countries_by_year(valor)
 
-# Valor acumulado do 5 maiores de 1970-2022
+# Evolution of the top 5 countries that export the most wine to Brazil over time
 group5 = top5.groupby(['País'])['Valor(US$)'].sum().sort_values(ascending=False).reset_index()
 group5 =  group5.head(5)
 
-st.title('Análise da importação brasileira de vinhos de mesa')
+st.title('Analysis of Brazilian Importation of Table Wines')
 
-# organizando o dash
+# organizing the dashboard
 row1 = st.columns(2)
 row2 = st.columns(2)
 
-# coluna 1
+# column 1
 fig_group5_total = px.bar(group5, x="País", y='Valor(US$)',text=group5['Valor(US$)'].apply(lambda x: f'{x:,.0f}'), color_discrete_sequence=px.colors.sequential.RdBu,
-                          title='Valor acumulado dos 5 maiores exportadores de vinho para o Brasil entre 1970-2022')
+                          title='Accumulated value of the top 5 wine exporters to Brazil between 1970-2022')
 fig_group5_total.update_layout(width=600,  height=460)
 fig_group5_total.update_traces(textposition='outside', textfont=dict(color='black'))
 
 with row1[0]:
      st.write(fig_group5_total)
 
-# coluna 2
+# column 2
 fig_vl_anual = px.line(valor_anual, x="Ano", y='Valor(US$)', color_discrete_sequence=px.colors.sequential.RdBu,
-                       title='Valor anual de vinho importado pelo Brasil' )
+                       title='Annual value of table wine imported by Brazil' )
 fig_vl_anual.update_xaxes(dtick=2)
 fig_vl_anual.update_yaxes(dtick=50000000)
 fig_vl_anual.update_layout(width=600,  height=460)
@@ -64,7 +64,7 @@ with row1[1]:
      st.write(fig_vl_anual)
 
 
-# coluna 3
+# column 3
 filtro_ano_top5 = top5[(top5['Ano'] >= 1999) & (top5['Ano'] <= 2022)]
 
 fig_top5 = px.scatter(filtro_ano_top5, x='Ano', y='Valor(US$)',
@@ -79,11 +79,11 @@ fig_top5 = px.scatter(filtro_ano_top5, x='Ano', y='Valor(US$)',
                  },
                   range_x=[1999, 2023], range_y=[0, 250000000])
 
-# Personalizar o layout do gráfico
+#  chart layout
 fig_top5.update_layout(
-    title='Evolução dos 5 países que mais exportam vinho para o Brasil ao longo do tempo',
+    title='Evolution of the top 5 countries that export the most wine to Brazil over time',
     xaxis_title='Ano',
-    yaxis_title='Quantidade em Dólares',
+    yaxis_title='(US$)',
     legend_title='Países'
 )
 fig_top5.update_yaxes(dtick=15000000)
